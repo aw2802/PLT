b5ed9dd9d72b18b0d39c286331d8842297a48ddf
Zeynep Ejder <zeynepejder@dyn-160-39-119-48.secure.barnard.edu>
2016-11-15 20:13:04 -0500

    Trying very hard

--- codegen.ml
+++ codegen.ml
@@ -11,3 +11,5 @@ exception Error of string
 let context = global_context ()
 let the_module = create_module context "javapm"
 let builder = builder context
+
+let stubby parameter = function
--- javapm.ml
+++ javapm.ml
@@ -1,7 +1,12 @@
 
-  
+let filename = Sys.argv.(1) ^ ".javapm" in
 
-let _ =
-  let lexbuf = Lexing.from_channel stdin in
-  let expr = Parser.program Scanner.token lexbuf in
-  print_endline (string_of_int expr)
+let in_channel = open_in Sys.argv.(1) in
+
+let lexbuf = Lexing.from_channel in_channel in
+
+let program = Parser.program Scanner.token lexbuf in
+let outprog = Codegen.stubby program in
+
+let headers = outprog 
+		in Printf.fprintf (open_out filename)  headers
