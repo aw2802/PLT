4ab5acf0486c2bd74f5dc84f87c81f3b0b032e28
adaguanno <ashley.daguanno@gmail.com>
2016-11-18 19:16:11 -0500

    Update codegen.ml

--- codegen.ml
+++ codegen.ml
@@ -8,8 +8,32 @@ open Hashtbl
 open Ast
 exception Error of string
 
+module L = Llvm
+module A = Ast
+
 let context = global_context ()
 let the_module = create_module context "javapm"
 let builder = builder context
 
 let stubby parameter = "stubed"
+
+let i32_t = L.i32_type context;; (* integer *)
+let i8_t = L.i8_type context;; (* printf format string *)
+let i1_t = L.i1_type context;; (* boolean *)
+let i64_t = L.i64_type context ;; (* idk *)
+let f_t = L.double_type context;; (* float *)
+
+let str_t = L.pointer_type i8_t;; 
+let void_t = L.void_type context;; (* void *)
+
+let rec get_llvm_type datatype = match datatype with (* LLVM type for AST type *)
+	  A.JChar -> i8_t
+	| A.JVoid -> void_t
+	| A.JBoolean -> i1_t
+	| A.JFloat -> f_t
+	| A.JInt -> i32_t
+	| _ -> raise(Failure("Invalid Data Type"))
+	(** | A.Object --> @TODO **) 
+
+
+		
