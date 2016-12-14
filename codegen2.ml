3db0fcc3ea6689122472ba1048cf7fa4a6f6019a
Zeynep Ejder <zeynepejder@dyn-160-39-118-149.secure.barnard.edu>
2016-12-14 12:51:27 -0500

    fixing compilation

--- codegen2.ml
+++ codegen2.ml
@@ -121,7 +121,6 @@ let translate sast =
 		L.build_store value var llbuilder
 *)
 	and assign_to_variable vmemory e2 llbuilder =
-		let temp = 
 		let value = match e2 with
 		| SId(id, d) -> ignore(L.build_load (expr_gen llbuilder e2) temp llbuilder); temp
 		| _ -> expr_gen llbuilder e2
