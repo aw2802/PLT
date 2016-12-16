6cb7930674b202092f61db56f8140607f36be33c
annawen12@gmail.com <aw2802@barnard.edu>
2016-12-15 21:41:04 -0500

    adding arrays

--- sast.ml
+++ sast.ml
@@ -16,13 +16,13 @@ type sexpr =
 	| SBinop of sexpr * op * sexpr * data_type
 	| SAssign of string * sexpr * data_type
 	| SNoexpr
-	| SFuncCall of string * sexpr list * data_type * int (**check me y tho**)
+	| SFuncCall of string * sexpr list * data_type * int 
 	| SUnop of op * sexpr * data_type
 	| SCreateObject of string * sexpr list * data_type
-(**	| SArrayCreate of data_type * sexpr list * data_type
+	| SArrayCreate of data_type * sexpr list * data_type
 	| SArrayAccess of sexpr * sexpr list * data_type
 	| SArrayElements
-	| SObjAccess of sexpr * sexpr * data_type **)
+	| SObjAccess of sexpr * sexpr * data_type *
 
 type svdecl = {
 	svscope: scope;
