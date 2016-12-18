1e027d22958aa5b89a3a246a97624a72d72cb580
adaguanno <ashley.daguanno@gmail.com>
2016-12-18 16:59:15 -0500

    Fix codegen

--- codegen2.ml
+++ codegen2.ml
@@ -249,8 +249,8 @@ let translate sast =
 		let allocatedMemory = L.build_alloca struct_type vname llbuilder in
 		List.iteri (
 			fun i f ->
-	        let tuple_value = L.build_struct_gep allocatedMemory i ("temp"^i) llbuilder in
-	        	L.build_store (expr_gen llbuilder f) tuple_value llbuilder;
+	        let tuple_value = L.build_struct_gep allocatedMemory i "temp" llbuilder in
+	        	ignore(L.build_store (expr_gen llbuilder f) tuple_value llbuilder);
 	    	) 
 	    expr_list; 
 		L.build_pointercast allocatedMemory (L.pointer_type struct_type) "tupleMemAlloc" llbuilder
