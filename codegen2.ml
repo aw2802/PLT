526d2a6d02e3a4ce2ddd03b78a6259762afcc83a
zeynepejder <zeynep@ejder.name.tr>
2016-12-20 00:57:41 -0500

    fixing bool print

--- codegen2.ml
+++ codegen2.ml
@@ -440,11 +440,15 @@ let translate sast =
 		let printf = find_func_in_module "printf" in
 		let map_expr_to_printfexpr expr = match expr with
 			| SId(id, d) -> (match d with
-							| A.JBoolean -> let value = (get_value true id llbuilder) in
-											(match value with 
-											| boolean_False -> (expr_gen llbuilder (SString_Lit("false"))) 
-								  			| boolean_True -> (expr_gen llbuilder (SString_Lit("true")))
-								  			| _ -> raise (Failure("cannot match boolean")))
+							| A.JBoolean -> let tmp_var = "print_bool" in
+											let trueStr = SString_Lit("true") in
+											let falseStr = SString_Lit("false") in
+											let id = SId(tmp_var, str_type) in 
+											ignore(stmt_gen llbuilder (SLocalVarDecl(str_type, tmp_var, SNoexpr)));
+											ignore(stmt_gen llbuilder (SIf(expr, 
+											SExpr(SAssign(id, trueStr, str_type), str_type), 
+											SExpr(SAssign(id, falseStr, str_type), str_type))));
+											expr_gen llbuilder id
 							| _ -> get_value true id llbuilder)
 			| STupleAccess(e1, e2, d) -> generate_tuple_access true e1 e2 llbuilder 
 			| SBoolean_Lit (b) ->	if b then (expr_gen llbuilder (SString_Lit("true"))) else (expr_gen llbuilder (SString_Lit("false")))
