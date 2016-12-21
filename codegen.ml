7fa80acd861c68b333f3124cb173edf8d37af433
zeynepejder <zeynep@ejder.name.tr>
2016-12-20 20:06:55 -0500

    object access

--- codegen.ml
+++ codegen.ml
@@ -109,7 +109,6 @@ let translate sast =
 		List.iteri (
 			fun i f ->
 	        let n = c.scname ^ "." ^ f in
-	        print_string(n);
 	        Hashtbl.add struct_field_idx_table n i;
 	    	) 
 	    name_list; 
